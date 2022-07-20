7bafd586ba16ae13bc5a110af99f22e6a62ce6a1
epos95 <maxagnesund95@gmail.com>
2022-07-20 13:41:37 +0200

    updates things

--- build.py
+++ build.py
@@ -5,11 +5,18 @@ import sys
 import json
 import os
 
+# Populate DATA with the predetermined data from data.json
 with open("data.json", "r") as f:
     DATA = json.load(f)
 
-def get_html(fname):
+# Read header into DATA
+with open("utils/header.html") as f2:
+    DATA["header"] = f2.read()
+    #DATA["HEADER"] = DATA["header"].replace("{{data['date']}}", "TEST")
 
+
+# Gets the rendered html string from a html document
+def get_html(fname):
     with open(f"templates/{fname}", "r") as f:
         file_contents = f.read()
 
@@ -18,6 +25,7 @@ def get_html(fname):
 
     return template.render(data = DATA)
 
+# Puts data regarding all the articles into DATA
 def populate_articles():
     DATA["articles"] = []
 
@@ -34,15 +42,19 @@ def populate_articles():
         DATA["articles"].append(art)
 
 if __name__ == "__main__":
-
     populate_articles()
-    print(f"Found articles: {DATA['articles']}")
 
-    # Create html for the articles
+    # Print the articles we are about to publish
+    print("Found articles:")
+    for article in DATA["articles"]:
+        print(f" * href: {article['link']}\n   name: {article['name']}\n")
+
+    # Create html for the articles and write it too disk for easy access when publishing
     articles = os.listdir("templates")
     for article in articles:
         html = get_html(article)
 
         with open(article, "w+") as f:
             f.write(html)
-            print(f"Created {article}")
+
+        print(f"Created {article}")
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
-# Work in progress!
+# My blog / home page 
+
+## How to use:
+1. Write a org document *somewhere*
+2. Use `epos/blog-publish` in emacs to export the org document as HTML to `~/Blog/templates/`
+3. After building the project you can use `just build` and `just run` to proofread the generated HTML.
+4. After this `just publish` can be used to push the new generated articles to github.
+5. Now the articles should be availible on the [blog](https://epos95.github.io/)
+
